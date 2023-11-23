c75cacfd8d490998cccc045962f5680ae8573632
khanh.nvm <minhbi245@gmail.com>
2023-11-23 12:00:23 +0700

    add method input student list and create sortingStudentList

--- Java/OOP/Session05-Array/BasicArray/src/khanh/stage/Sorting.java
+++ Java/OOP/Session05-Array/BasicArray/src/khanh/stage/Sorting.java
@@ -1,8 +1,92 @@
 package khanh.stage;
 
+import khanh.data.Student;
+
+import java.util.Scanner;
+
 public class Sorting {
     public static void main(String[] args) {
-        sortPrimitiveV2();
+        inputStudentList();
+    }
+
+    // CHƯA OOP 100%, MỌI THỨ PHẢI THUỘC VỀ OBJECT, ĐI QUA OBJECT CHẤM!!!
+    public static void inputStudentList() {
+        Student[] arr;  // Chưa biết bao nhiêu sv
+        // Có thể khai báo 500 sv có sẵn 500 chỗ
+        // Có thể hỏi muốn bao nhiêu, thì new bấy nhiêu
+        // Có 1 cách xịn sò hơn, khỏi hỏi bao nhiêu, đưa new Student() vào
+        // là có chỗ, mảng linh hoạt, co giãn kích thước - Collections Framework
+        int count;
+        String id, name;
+        int yob;
+        double gpa;
+
+        System.out.println("How many student do you want to input? ");
+        Scanner sc = new Scanner(System.in);
+//        count = sc.nextInt(); // Trôi lệnh, try-catch, MyToys đưa vào
+        count = Integer.parseInt(sc.nextLine());
+        System.out.println("Input id: ");
+        arr = new Student[count];
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println("Input student #" + (i + 1) + "/" + count);  // ngoài đời đếm từ 1, lừa đảo chỉ số i 1 tí
+                                                                            // i chạy như cũ, đếm đôn i lên + 1
+            System.out.println("Input your id: ");
+            id = sc.nextLine();
+
+            System.out.println("Input your name: ");
+            name = sc.nextLine();
+
+            System.out.println("Input your yob: ");
+            yob = Integer.parseInt(sc.nextLine());  // MyToys
+
+            System.out.println("Input your gpa: ");
+            gpa = Double.parseDouble(sc.nextLine());    // MyToys.
+
+            arr[i] = new Student(id, name, yob, gpa);
+        }
+        System.out.println("The student list");
+        for (Student x: arr) {
+            x.showProfile();
+        }
+    }
+
+    public static void sortStudentList() {
+//        Student[] arr = new Student[5];
+//        arr[0] = new Student("SE9", "CHÍN", 19, 9.0);
+//        arr[1] = new Student("SE5", "NĂM", 15, 5.0);
+//        arr[2] = new Student("SE8", "TÁM", 18, 8.0);
+//        arr[3] = new Student("SE2", "HAI", 12, 2.0);
+//        arr[4] = new Student("SE7", "BẢY", 17, 7.0);
+        Student[] arr = new Student[]{
+                new Student("SE9", "CHÍN", 19, 9.0),
+                new Student("SE5", "NĂM", 15, 5.0),
+                new Student("SE8", "TÁM", 18, 8.0),
+                new Student("SE2", "HAI", 12, 2.0),
+                new Student("SE7", "BẢY", 17, 7.0)
+        };
+        System.out.println("The list student before sorting");
+        for (Student x : arr) {
+            x.showProfile();
+        }
+
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i].getGpa() > arr[j].getGpa()) {
+                    Student tmp = arr[i];   // Điểm 0 ổn, thì đổi gọi tên, trỏ
+                    arr[i] = arr[j];
+                    arr[j] = tmp;
+                } // Hóa đổi việc trỏ đúng vào vùng new có data điểm phù hợp
+                // Thứ tự, đổi việc/thứ tự gọi tên sinh viên
+                // Thay vì gọi điểm cao, gọi điểm thấp trước, vẫn gọi từ [0] đầu danh sách
+            }
+        }
+
+        System.out.println("The list student after sorting ascending by gpa");
+        for (Student x : arr) {
+            x.showProfile();
+        }
+
     }
 
     public static void sortPrimitiveV2() {
@@ -16,9 +100,9 @@ public class Sorting {
 
         for (int i = 0; i < arr.length - 1; i++) {
             for (int j = i + 1; j < arr.length; j++) {
-                if(arr[i] > arr[j]) {
-                    int tmp = arr[i];
-                    arr[i] = arr[j];
+                if (arr[i] > arr[j]) { // i luôn nhỏ hơn j, trước j truong mảng
+                    int tmp = arr[i]; // đứng trước mà bày đặt lớn hơn
+                    arr[i] = arr[j];  // thì swap thôi
                     arr[j] = tmp;
                 }
             }
