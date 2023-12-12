d0effb6f6d5db4fd39be4180df90658a108ded45
khanh.nvm <minhbi245@gmail.com>
2023-12-12 18:09:54 +0700

    Done theory Collections, create project and data for Student Management

--- Java/OOP/Session08-Collections_Framework/StudentManagement/src/Main.java
+++ Java/OOP/Session08-Collections_Framework/StudentManagement/src/Main.java
@@ -1,5 +1,49 @@
+import data.Student;
+
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
 
+    }
+
+    public static void playWithArrayList() {
+        // Student[] arr = new Student[500];
+        // arr là 1 cái túi chứa sẵn 500 chỗ, 500 sinh viên/món đồ có thể bỏ vô
+        // moón đồ phải đi mua, new, rồi thảy vào
+        // tên mảng chính là má mì, quản lí nhiều đồ, tên túi luôn
+
+        //List arr = new ArrayList(); // đựng lộn xộn đủ loại con trỏ
+        // đúng nghĩa là túi Doraemon, món gì cũng dưng, đựng tham chiếu hoy nha
+        // nhét được cả NhanVien, GiangVien, Animal, Dog, ...
+
+
+        // Túi đồng nhất chỉ dùng lưu con trỏ Student
+        List<Student> arr = new ArrayList<>();  // túi đựng đồng nhất 1 loại gọi là GENERIC
+        //List<Student> arr1 = new ArrayList<Student>()
+        // arr là cái túi chứa vô hạn chỗ, vô hạn con trỏ
+        // nhét đồng nhất món đồ dễ chấm chung được, đa hình được
+
+        // MƠỞ NGĂN TỦ/TÚI, NHÉT ĐỒ VÔ
+        //arr[i] = new Student(...); // mảng
+        //arr.add(new Student(...));  // lưu con trỏ hoy
+                                    // new Student thoải mái ở bên ngoài, vùng HEAP
+
+        Student s1 = new Student("SE123456", "AN NGUYỄN", 2003, 7.5);
+        Student s2 = new Student("SE123457", "BÌNH LÊ", 2003, 8.5);
+        arr.add(s1);    // giỏ vùng new ArrayList, tủ có thêm 1 con trỏ, sinh ra 1 con trỏ
+        arr.add(s2);    // sinh ra thêm con trỏ nữa [1]
+                        // add() vô tận, mảng [lenght - 1] hết mức
+                        // new cứ nằm ở HEAP, SV cứ ở nhà, công nhân cứ ở xưởng, bệnh nhân cứ ở phòng
+        // add() ở đây là mở khóa kéo, khóa dây, nhét đồ vô, nhét tham chiếu, thẻ bài
+        // -> sinh ra con trỏ
+        arr.add(s1);    // add trùng nè 1 tên người nữa lại vào danh sách,
+        // Set ko báo lỗi đâu, nhưng chỉ lấy đúng 1... đừng đi vào chi tiết vội vã
+
+        arr.add(new Student("SE999999", "CHIN PHẠM", 2003, 9.9));
+        //      new cứ tạo ra object lên HEAP mà nằm, bệnh nhân cứ về phòng, tớ nắm đươ
+        for (Student x: arr) {
+            x.showProfile();
+        }
     }
 }
---
+++ Java/OOP/Session08-Collections_Framework/StudentManagement/src/data/Student.java
@@ -0,0 +1,70 @@
+package data;
+
+/*
+    Nhắc đến sinh viên, chỉ cần quan tâm mã số, tên, năm sinh, chuyên ngành, điểm trung bình
+    => chính là ABSTRACTION
+    biểu diễn được đối tượng này như ngoài đời thực, ta cần Khuôn + đặc điểm + haành vi
+    Che giấu show ra khi cần thiết -> ENCAPSULSATION, gói trọn trong 1 object
+
+    abstract class là sự phân loại, rút gọn hơn, còn Abstraction là rút gọn thông tin, tổ chức lại thông tin
+ */
+
+public class Student {
+    private String id;
+    private String name;
+    private int yob;
+    private double gpa;
+
+    public Student(String id, String name, int yob, double gpa) {
+        this.id = id;
+        this.name = name;
+        this.yob = yob;
+        this.gpa = gpa;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getYob() {
+        return yob;
+    }
+
+    public void setYob(int yob) {
+        this.yob = yob;
+    }
+
+    public double getGpa() {
+        return gpa;
+    }
+
+    public void setGpa(double gpa) {
+        this.gpa = gpa;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", yob=" + yob +
+                ", gpa=" + gpa +
+                '}';
+    }
+
+    public void showProfile() {
+        System.out.printf("|%8s|%-25s|%4d|%4.1f|", id, name, yob, gpa);
+    }
+}
