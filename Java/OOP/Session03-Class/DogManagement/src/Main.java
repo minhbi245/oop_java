a431f8c14b0f072804d75063b1c0afbee249cda0
khanh.nvm <minhbi245@gmail.com>
2024-04-26 23:32:19 +0700

    add new gitignore and updatre the theory about the object - Behavior - Property - Get and Set value of property

--- Java/OOP/Session03-Class/DogManagement/src/Main.java
+++ Java/OOP/Session03-Class/DogManagement/src/Main.java
@@ -2,10 +2,22 @@ import data.Dog;
 
 public class Main {
     public static void main(String[] args) {
-        Dog cauVang = new Dog("Cậu vàng", 5, 5.5);
-        cauVang.getInfo();
+        // biến    value đơn
+        int age = 20;
+//    int là primitive data type
+        //  biến       value phức tạp, object
+        Dog chiHu = new Dog("Chi Hu Hu", 2021, 2.5);
+        // Dog được gọi là Object Data Type
+        chiHu.bark();
 
-        cauVang.setWeight(7.0);
-        cauVang.getInfo();
+        Dog ngaoDa = new Dog("NGÁO ĐÁ", 2020, 50.0);
+        ngaoDa.bark();
     }
 }
+
+// new: là một hành động dùng để clone ra một object dựa trên class blueprint trên một vùng không gian trống
+// new còn là cách ta xin vùng RAM mới, để đổ data vào vùng mơ
+// Như ngoài đời thật là lấy phôi CMND/CCCD các chỗ điền còn trống, đổ dữ liệu, điền dữ liệu thông tin vào
+// new tương đương malloc() hay là calloc()
+// malloc() ~ memory allocation
+// calloc() ~ clear memory allocation
--- Java/OOP/Session03-Class/DogManagement/src/data/Dog.java
+++ Java/OOP/Session03-Class/DogManagement/src/data/Dog.java
@@ -9,16 +9,39 @@ public class Dog {
     private double weight;
 
     // Một cái khuôn/Class cần cái phễu để hứng vật liệu/giá trị đưa vào
+    // Phễu/Constructor/Initializer hay còn lại là hàm dựng được sử dụng để đúc ra một Object thuộc Class/Khuôn tương ứng
+    // Constructor/Initializer sẽ return chính các object đang bàn
+    // Do đó ta không cần ghi return và giá trị trả về
+    // Tên của Constructor/Initializer giống 100% tên của class đối với Java
+    // Mỗi lần mà ta gọi Phễu/Constructor/Initializer tương đương với 1 lần tạo/đúc ra một object
+    // Lúc này có 1 object mới hoàn toàn được tạo ra với vật liệu/giá trị được đổ vào
+    // Giống như ba má sinh ra một đứa em, cho nó tên:...; cân nặng:...; ngày sinh:...
     public Dog(String name, int yob, double weight) {
         this.name = name;
         this.yob = yob;
         this.weight = weight;
     }
 
+    // Các hàm của object là những quy tắc để xử lý dữ liệu thông tin
+    // Một object (thực thể sống như tôi & bạn) có nhiều hành động hằng ngày, hành động có liên quan đến dữ liệu/data
+    // Hàm chính là sẽ xử lý trên data trên đặc điểm hay thuộc tính của một object
+    // Hàm trong object còn được gọi là method - behaviors(hành vi)
+
+    public void bark() {
+//        System.out.println("gogogogo..., my name is " + name);
+//        System.out.println("gogogogo..., my yob is " + yob);
+        System.out.printf("|GAU GAU|%-15s|%4d|%4.1f|\n", name, yob, weight);
+    }
+
+    // Khi ta đã có info, ta sẽ có nhu cầu giao tiếp cách nào đó để lấy info này ra, dưa ra những thông tin này
+    // Thông tin này sẽ được đưa ra khi ai đó cần hoặc hỏi lấy nhằm mục đích khác
+    // Việc này trong máy tính thì ta sẽ pha xử lí lấy ra thông tin thông qua hàm gọi là get-thông tin()
     public String getName() {
         return name;
     }
 
+    // Nhìn cấu trúc có tham số parameter truyền vào, tuy nhiên nó lại không phải là phễu.
+    // Phễu hay constructor được sử dụng để tạo ra một Object mới, còn đây là dựa vào object đã được tạo và ta sẽ thiết lập lại thuộc tính/thông tin của Object đó
     public void setName(String name) {
         this.name = name;
     }
@@ -40,6 +63,13 @@ public class Dog {
     }
 
     public void getInfo() {
-        System.out.printf("|%25s|%10d|%10.2f|\n", name, yob, weight);
+        System.out.printf("|%-25s|%10d|%10.2f|\n", name, yob, weight);
     }
 }
+
+// Nếu ta viết các dữ liệu này bên ngoài thì tương đương với ngoài đời là cái Khuôn bị lủng
+
+// Biến/Data/Properties/Thuộc tính + Hàm/Method để xử lí data đó, HIÌNH THÀNH NÊN CHẤT CỦA 1 OBJECT CỤ THỂ NÀO ĐÓ
+// N THỨ GÓI LẠI TRONG 1 KHUÔN/OBJECT
+// KHÔNG LỘN XỘN GIỮA DOG CAT HAY PERSON, GIÁO VIÊN, SINH VIÊN, SÁCH...
+// LÀ MỘT KĨ THUẬT GOM HÀM + BIẾN VÀO 1 KHUÔN/OBJECT, HÌNH THÀNH NÊN 1 THỨ RIÊNG BIỆT -> CHÍNH LÀ TÍNH CHẤT ĐÓNG GÓI ENCAPSULATION
